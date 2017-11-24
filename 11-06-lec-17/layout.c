bbb2ec500c48f499eea0ed21dfc80a1e155be34b
Joe Gibbs Politz <joe.politz@gmail.com>
2017-11-24 14:39:54 -0800

    .

---
+++ 11-06-lec-17/layout.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <stdio.h>
+
+int main(int argc, char** args) {
+  int i;
+    for(i = 0; i < argc; i += 1) {
+        printf("%s\n", args[i]);
+          }
+            return 0;
+            }
